84a7bd31ee603551803535048b35fc7bda521a1e
BADD0G <christophercheng1231@gmail.com>
2023-06-30 20:44:04 -0700

    site nav landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SiteNav from "./components/sitenav/SiteNav";
+import Landing from "./components/landing/Landing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SiteNav />
+      <Routes>
+        <Route path="./" element={<Landing />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/sitenav/SiteNav.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function SiteNav() {
+  return (
+    <React.Fragment>
+      <nav
+        className="navbar navbar-expand-md navbar-dark bg-dark"
+        aria-label="Fourth navbar example"
+      >
+        <div className="container">
+          <Link
+            to="/"
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample04"
+            aria-controls="navbarsExample04"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </Link>
+
+          <div className="collapse navbar-collapse" id="navbarsExample04">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link to="./" className="nav-link px-2 text-white link-button">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/DailyUpdate"
+                  className="nav-link px-2 text-white link-button"
+                >
+                  Daily Update
+                </Link>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="/"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Options
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="/MittsAndDice">
+                    Request help
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="/whiteelephanttwist">
+                    Connect
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="text-end">
+            <a
+              href="/"
+              className="align-items-center mb-2 me-2 mb-lg-0 text-white text-decoration-none"
+            >
+              User
+            </a>
+            <Link
+              to="login"
+              type="button"
+              className="btn btn-outline-light me-2"
+            >
+              Login
+            </Link>
+            <Link to="register" className="btn btn-info">
+              Register
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <div className="divider p-1"></div>
+    </React.Fragment>
+  );
+}
+
+export default SiteNav;
